bedb5ba45204604462815d4159463f3cf736a620
aloksomani <spsomani@gmail.com>
2015-05-10 23:49:47 -0700

    formats index grid, adds profile filter

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -1,11 +1,16 @@
 class CamerasController < ApplicationController
 
   def index
-      @cameras = Camera.all
+    @cameras = Camera.all
   end
 
   def show
-      @camera = Camera.find(params[:id])
+    @camera = Camera.find(params[:id])
+  end
+
+  def filtered_index
+    @cameras = Camera.joins(:profiles).where(profiles: { style: params[:style] } )
+    # @cameras = Camera.where(resolution: params[:res])
   end
   
 end
