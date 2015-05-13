2277d961eb4713a347ccd13acccc8bd0da374c3b
aloksomani <spsomani@gmail.com>
2015-05-13 16:19:24 -0700

    adds index filtering

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -1,8 +1,29 @@
 class CamerasController < ApplicationController
 
   def index
-    if params[:style]
-      @cameras = Camera.joins(:profiles).where(profiles: {style: params[:style]} ).where(brand: params[:brand])
+
+    profile = params[:profile] || nil
+    portability = params[:portability] || 1
+
+    if params[:price] == "600"
+      price_start = 0
+      price_end = 600
+    elsif params[:price] == "1200"
+      price_start = 601
+      price_end = 1200
+    elsif params[:price] == "2100"
+      price_start = 1201
+      price_end = 2100
+    else
+      price_start = 0
+      price_end = 2100
+    end
+
+
+    if (profile == nil)
+      @cameras = Camera.where("portability >= ?", portability).where(price: price_start..price_end)
+    elsif (profile != nil)
+      @cameras = Camera.joins(:profiles).where(profiles: {style: profile[:style]} ).where("portability >= ?", portability).where(price: price_start..price_end)
     else
       @cameras = Camera.all
     end
